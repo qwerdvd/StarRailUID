72c35ded19d38beecf8abf959687f7d5e876e998
twfx1207 <twfx1207@gmail.com>
2023-05-26 11:12:04 +0900

    fix server name

--- StarRailUID/utils/mys_api.py
+++ StarRailUID/utils/mys_api.py
@@ -23,11 +23,11 @@ from ..sruid_utils.api.mys.models import (
 RECOGNIZE_SERVER = {
     '1': 'prod_gf_cn',
     '2': 'prod_gf_cn',
-    # '5': 'cn_qd01',
-    '6': 'os_usa',
-    '7': 'os_euro',
-    '8': 'os_asia',
-    '9': 'os_cht',
+    '5': 'prod_qd_cn',
+    '6': 'prod_official_usa',
+    '7': 'prod_official_euro',
+    '8': 'prod_official_asia',
+    '9': 'prod_official_cht',
 }
 
 
